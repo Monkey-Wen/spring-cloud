0af1622b41715b05a3d04381cbd12b3ff3ae1d7b
MonkeyWen <wenmengfei@newings.net.cn>
2018-11-29 17:35:40 +0800

    haha

---
+++ eureka/src/main/java/com/monkey/eureka/web/EurekaController.java
@@ -0,0 +1,21 @@
+package com.monkey.eureka.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("dc")
+    public String getDC(){
+
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+}
